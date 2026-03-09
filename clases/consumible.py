0fd858d0960c695db8ec6f80ea6ba444171b7bb9
hugestbel <heb9@alu.ua.es>
2026-03-09 13:21:08 +0100

    Evitar liada

--- clases/consumible.py
+++
@@ -1,7 +0,0 @@
-from item import Item
-
-class Consumible(Item):
-
-    def __init__(self, nombre, fecha_vencimiento, stock=0):
-
-        super().__init__(nombre, fecha_vencimiento, stock)
--- clases/item.py
+++
@@ -1,7 +0,0 @@
-class Item():
-
-    def __init__(self, nombre, fecha_vencimiento, stock=0):
-
-        self.nombre = nombre
-        self.stock = stock
-        self.fecha_vencimiento = fecha_vencimiento
