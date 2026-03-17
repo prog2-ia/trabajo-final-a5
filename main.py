4768bed615d0440f995df9ce279d5bcc371bedad
hugestbel <heb9@alu.ua.es>
2026-03-17 22:30:58 +0100

    Creo el archivo funciones
    Este lo usaremos para organizar funciones que usaremos en main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # Importar clases y funciones necesarias
+import funciones
+
+# Primera capa de menu
+def menu_principal():
+
+
+    print(
+        f'\n\t1 -\tAlmacen\n'
+        f'\t2 -\tEquipamiento\n'
+        f'\t3 -\tConsumibles\n'
+        f'\t0 -\tSalir programa\n'
+    )
+
+    # Pedir la instrucción
+    return pedirNum('\nAcceder a: ')
+    
 
 
 if __name__ == '__main__':
