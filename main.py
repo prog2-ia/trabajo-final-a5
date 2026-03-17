197a531d624a80b090841c11af4cb30a1a4aa3f6
hugestbel <heb9@alu.ua.es>
2026-03-17 22:18:46 +0100

    Crear fichero para_hacer e indicar cosas para hacer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Importar clases y funciones necesarias
 
-from clases import Item, Consumible, Equipo
 
 if __name__ == '__main__':
     
     print('Aquí ira el flujo del código...')
-    print('')
