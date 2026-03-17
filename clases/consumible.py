79027aaf27a114c2576ec6d54ce5c9df0a3f07f5
hugestbel <heb9@alu.ua.es>
2026-03-17 21:57:47 +0100

    Modifico la clase consumible

--- clases/consumible.py
+++ clases/consumible.py
@@ -1,6 +1,20 @@
 from item import Item
 from almacen import Inventario
+from lote import Lote
 
 class Consumible(Item):
 
-    
+    def __init__(self, nombre: str, inventario: Inventario, cantidad: int, lote: Lote):
+
+        super().__init__(nombre, inventario, cantidad)
+        self.__lote = lote  # No se podra mofidicar
+                            # El lote de un consumible representa su fecha de vencimiento
+                            # Entonces no tendrá setter
+
+    def __str__(self):
+
+        pass
+
+    @property   # Getter del lote del consumible
+    def lote(self):
+        return self.__lote
---
+++ clases/lote.py
@@ -0,0 +1,5 @@
+class Lote():
+
+    def __init__(self, fecha_vencimiento):
+
+        self.fecha_vencimiento = fecha_vencimiento
