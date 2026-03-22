be2d33b5fa1759a70d567bf6d743fecdbbc21091
Pichulenge <mpb131@alu.ua.es>
2026-03-22 11:02:42 +0100

    clase termico

---
+++ clases/Termico.py
@@ -0,0 +1,10 @@
+from equipo import Equipo
+
+class Termico(Equipo):
+    def __init__(self, nombre, inventario, cantidad, estado, temp_max: float, temp_min: float):
+        super().__init__(nombre, inventario, cantidad, estado)
+        self.temp_max = temp_max
+        self.temp_min = temp_min
+
+    def __str__(self):
+        return super().__str__() + f" | Rango: {self.temp_min}º a {self.temp_max}º"
