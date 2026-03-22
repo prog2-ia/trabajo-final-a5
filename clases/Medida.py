f7862df061c2b49f11fae7dfb11a5affa3b01b54
Pichulenge <mpb131@alu.ua.es>
2026-03-22 11:01:02 +0100

    clase medida

---
+++ clases/Medida.py
@@ -0,0 +1,9 @@
+from equipo import Equipo
+
+class Medida(Equipo):
+    def __init__(self, nombre, inventario, cantidad, estado, error_medida: float):
+        super().__init__(nombre, inventario, cantidad, estado)
+        self.error_medida = error_medida
+
+    def __str__(self):
+        return super().__str__() + f" | Error: ±{self.error_medida}"
