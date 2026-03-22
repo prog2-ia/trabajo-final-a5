055ee009d858b263e37d59d27a9a30aa08c140f4
Pichulenge <mpb131@alu.ua.es>
2026-03-22 11:06:35 +0100

    clase centrifugadora

---
+++ clases/Centrifugadora.py
@@ -0,0 +1,9 @@
+from equipo import Equipo
+
+class Centrifugadora(Equipo):
+    def __init__(self, nombre, inventario, cantidad, estado, rpm_max: int):
+        super().__init__(nombre, inventario, cantidad, estado)
+        self.rpm_max = rpm_max
+
+    def __str__(self):
+        return super().__str__() + f" | RPM Max: {self.rpm_max}"
