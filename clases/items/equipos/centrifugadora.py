9bf8c1a42a49c776f58b87acef70a141ffa55497
hugestbel <heb9@alu.ua.es>
2026-05-02 13:05:17 +0200

    funcion definir centrifugadora

--- clases/items/equipos/centrifugadora.py
+++ clases/items/equipos/centrifugadora.py
@@ -19,3 +19,40 @@ class Centrifugadora(Equipo):
             return super().__eq__(other) and self.rpm_max == other.rpm_max
         
         return False
+    
+
+
+def definir_centrifugadora(equipos: list) -> Centrifugadora:
+
+    # Se pasa la lista con equipo anteriormente definido
+
+    nombre = input('(-1) para cancelar la operación | Introduzca el nombre del equipo: ')
+
+
+    if nombre == '-1':
+
+        print('Cancelando operación...')
+        return '-1'
+
+
+    # Comprobramos que el nombre introducido forma parte o no
+    # de equipamiento anteriormente definido
+
+
+    for equipo in equipos:
+
+        if nombre.lower() == equipo.nombre:
+
+            print('Equipo anteriormente definido. Extrayendo copia...')
+
+            return equipo
+        
+    
+    rpm_max = pedir_int_entre_valores('Introduce las RPM máximas de la centrifugadora: ', 1, 100000)
+
+    if rpm_max == '-1':
+
+        print('Cancelando operación...')
+        return '-1'
+
+    return Centrifugadora(nombre.lower(), rpm_max)   # Todos los nombres serán puesto a lower
