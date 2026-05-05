1965a26b5cf6397d9f1436acd8985d13f3a98970
hugestbel <heb9@alu.ua.es>
2026-05-05 11:16:20 +0200

    Empiezo funcion de definir lote

--- clases/items/lote.py
+++ clases/items/lote.py
@@ -4,16 +4,18 @@ from funciones import *
 
 class Lote():
 
-    lotes = []  # Lista de lotes creados
 
-    def __init__(self, id_lote: str, fecha_vencimiento: date):
+    def __init__(self, id_lote: str, fecha_vencimiento: date, consumibles: list):
 
         # El id es constante, así que solo habrá getter
         self.__id_lote = id_lote
 
         # Las fechas son constante, así que solo habrá getter
         self.__fecha_vencimiento = fecha_vencimiento
-        Lote.lotes.append(self)
+
+        # Lista que contendrá los consumibles de un lote
+        self.consumibles = consumibles
+
 
 
     # Como no se puede modificar el id ni la fecha, no habrá setter, solo getter
@@ -38,3 +40,44 @@ class Lote():
             return self.id_lote == other.id_lote
         
         return False
+    
+# Los IDs de los lotes tendrán forman 00-ABC
+def comprobar_id_lote(id:str) -> bool:
+
+    if len(id) != 6:
+
+        return False
+    
+    return id[0:2].isdigit() and id[2] == '-' and id[3:6].isalpha()
+    
+
+
+# El usuario define un lote nuevo
+def definir_lote(lotes: list):
+
+    # Pasamos la lista de los lotes anteriormente definidos
+    # Hara falta para ver que no hayan IDs repetidos
+
+    while True:
+
+        introducir_id = input('(0) para cancelar la operación | Introduzca el ID (00-ABC) del nuevo lote: ').upper()
+
+        if introducir_id == '0':
+
+            return None
+        
+        if comprobar_id_lote(introducir_id):
+
+
+            # Comprobamos que no exista tal id en lotes anteriormente definidos
+            for lote in lotes:
+
+
+                if lote.id_lote == introducir_id:
+
+                    print('Operación cancelada. Ya existe un lote con ese ID.')
+
+
+
+        
+        print('Introduzca un ID válido o (0) para cancelar la operación.')
